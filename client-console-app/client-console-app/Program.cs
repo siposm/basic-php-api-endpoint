fb431b7cb30a31535f8c7542bd656ed0a98ae0fa
siposm <sipos.miklos@nik.uni-obuda.hu>
2020-04-21 15:20:26 +0200

    added console app client

---
+++ client-console-app/client-console-app/Program.cs
@@ -0,0 +1,44 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace client_console_app
+{
+    class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string Job { get; set; }
+        public int Salary { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Name} - {Age} - {Job} - {Salary}";
+        }
+    }
+
+    class DataFetcherService
+    {
+        public List<Person> FetchData()
+        {
+            string url = "https://siposm.hu/api/endpoint.php";
+            WebClient wc = new WebClient();
+            string x = wc.DownloadString(url);
+            return JsonConvert.DeserializeObject<List<Person>>(x);
+        }
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataFetcherService dfs = new DataFetcherService();
+            dfs.FetchData().ForEach(x => Console.WriteLine(x));
+        }
+    }
+}
